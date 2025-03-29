07572efec5d24cd873d6fc8ef1c6c2f7a5e21e7b
Zach Upstone <zu213@bath.ac.uk>
2025-03-29 22:53:56 +0000

    Added index page

--- src/App.js
+++ src/App.js
@@ -68,6 +68,8 @@ function App() {
 
           <Route path='/368squares' element={<Squares/>} />
 
+          <Route path='/index' element={<Index/>} />
+
           <Route path='*' element={<NotFound/>} />
         </Routes>
       </div>
@@ -256,6 +258,25 @@ function Squares() {
     width="100%"
     height="100%"
     />
+  </div>
+  )
+}
+
+function Index() {
+  return (
+  <div>
+    <div className='linkTitle'>Page links</div>
+    <div className='linksContainer'>
+      <div className='link'> <Link to="/">/home</Link></div>
+      <div className='link'> <Link to="/me">/me</Link></div>
+      <div className='link'> <Link to="/cv">/cv</Link></div>
+      <div className='link'> <Link to="/projects">/projects</Link></div>
+      <div className='link'> <Link to="/gallery">/gallery</Link></div>
+      <div className='link'> <Link to="/charts">/charts</Link></div>
+      <div className='link'> <Link to="/fruit-lips">/fruit-lips</Link></div>
+      <div className='link'> <Link to="/368squares">/368squares</Link></div>
+
+    </div>
   </div>
   )
 }
