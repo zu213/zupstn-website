9464c3a9c5c2791be134367d1b67e68094630e5a
Zach Upstone <zu213@bath.ac.uk>
2025-05-08 18:58:39 +0100

    Made the links and buttons on the site more synthwave styled

--- src/projects/TrafficSimulator.js
+++ src/projects/TrafficSimulator.js
@@ -14,7 +14,7 @@ function TrafficSimulator(props) {
           <br/>
           Users can input the number of entrances/exits the cars use, the rate of arrival at each entrance 
           and how many cars are let through with each light phase. As the simulator runs you can see if cars build
-          up at one particualt entrances and adjust how many cars should be let through accordingly.
+          up at one particular entrance and adjust how many cars should be let through accordingly.
           <br />
           <br />
           It utilises linking with <b>libwinpthread-1.dll</b> and management of C files by use of header files, 
