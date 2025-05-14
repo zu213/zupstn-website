911a355ee38dc4f870700928cbaaa2f28d14a307
Zach Upstone <zu213@bath.ac.uk>
2025-05-14 21:43:50 +0100

    Made graphic more square

--- public/hypno.sh
+++ public/hypno.sh
@@ -4,20 +4,15 @@
 ROWS=$(tput lines)
 COLS=$(tput cols)
 
-# Calculate center position
 START_ROW=$((ROWS / 2))
 START_COL=$((COLS / 2 ))
 
-# Animation settings
-# How many lines to move downward each cycle
-DELAY=0.0
+DELAY=0.0 # Delay for animation
 
 COLORS=(1 2 3 4 5 6)
 COLOR=1
-# Hide cursor
+# hide cursor
 tput civis
-
-# Trap Ctrl+C to restore cursor
 trap "tput cnorm; exit" INT
 
 clear
@@ -26,41 +21,35 @@ while true; do
     for ((i=0; i<START_ROW - 1; i++)); do
 
         ROW=$((START_ROW + i))
-        for ((j=-i; j<i; j += 2)); do
+        for ((j=-i * 2 + 2; j<i * 2; j += 2)); do
             COLUMN=$((START_COL + j))
 
             tput cup $ROW $COLUMN
             echo -e "=="
         done
 
-        MOD_COLUMN=$((START_COL + i))
+        MOD_COLUMN=$((START_COL + i * 2))
         for ((j=-i; j<i; j++)); do
             MOD_ROW=$((START_ROW - j))
             tput cup $MOD_ROW $MOD_COLUMN
-           echo -e "="
+           echo -e "=="
         done
 
         MOD_ROW=$((START_ROW - i))
-        for ((j=-i + 1; j<i; j += 2)); do
+        for ((j=-i * 2; j<i * 2; j += 2)); do
             MOD_COLUMN=$((START_COL - j))
 
             tput cup $MOD_ROW $MOD_COLUMN
             echo -e "=="
         done
 
-        MOD_COLUMN=$((START_COL - i))
-        for ((j=-i; j<i; j++)); do
+        MOD_COLUMN=$((START_COL - i * 2))
+        for ((j=-i; j<i + 1; j++)); do
             MOD_ROW=$((START_ROW + j))
 
             tput cup $MOD_ROW $MOD_COLUMN
-            echo -e "="
+            echo -e "=="
         done
-        
-        # Clear the previous line
-        #if (( i > 0 )); then
-        #    tput cup $((ROW - 1)) $START_COL
-        #    echo "$(printf ' %.0s' $(seq 1 $LINE_LENGTH))"
-        #fi
         
         sleep $DELAY
     done
@@ -70,7 +59,6 @@ while true; do
 
     tput setaf $COLOR
 
-    # Optional: reset to start
     tput cup $((START_ROW + HEIGHT - 1)) $START_COL
     echo "$(printf ' %.0s' $(seq 1 $LINE_LENGTH))"
 done
