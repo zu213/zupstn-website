3587d9d6e34312f77a398b351b474fe3e847ec1b
Zach Upstone <zu213@bath.ac.uk>
2025-05-18 20:47:33 +0100

    Working basic overhaul for curl aniamtaion

--- public/hypno.sh
+++ public/hypno.sh
@@ -1,67 +1,93 @@
 #!/bin/bash
 
-# Get terminal size
 ROWS=$(tput lines)
 COLS=$(tput cols)
-
-START_ROW=$((ROWS / 2))
-START_COL=$((COLS / 2 ))
-
-DELAY=0.0 # Delay for animation
-
-COLORS=(1 2 3 4 5 6)
-COLOR=1
-# hide cursor
+CENTER_ROW=$((ROWS / 2))
+CENTER_COL=$((COLS / 2))
+declare -a FRAMEBUFFER
+for ((i = 0; i < ROWS * COLS; i++)); do
+    FRAMEBUFFER[$i]=" "
+done
 tput civis
 trap "tput cnorm; exit" INT
 
-clear
+# Set a pixel at (row, col)
+set_pixel() {
+  local row=$1
+  local col=$2
+  local char=$3
+  if ((row >= 0 && row < ROWS && col >= 0 && col < COLS)); then
+    FRAMEBUFFER[$((row * COLS + col))]="="
+    if [ "$DIRECTION" -eq 1 ] ||[ "$DIRECTION" -eq 3 ]; then
+        FRAMEBUFFER[$((row * COLS + col + 1))]="="
+    fi
+  fi
+}
+
+# Render the framebuffer
+render() {
+    tput cup $CURRENT_ROW 0
+    line=""
+    for ((j = 0; j < COLS; j++)); do
+      line+="${FRAMEBUFFER[$((CURRENT_ROW * COLS + j))]}"
+    done
+    echo -ne "$line"
+}
+
+# Spiral drawing into buffer
+
+start_spiral() {
+    if [ "$DIRECTION" -eq 0 ]; then
+        CURRENT_COL=$((CURRENT_COL + 1))
+        set_pixel $CURRENT_ROW $CURRENT_COL
+        if (( CURRENT_COL - CENTER_COL >= 2 * COUNT  )); then
+            set_pixel $CURRENT_ROW $((CURRENT_COL + 1))
+            ((DIRECTION++))
+        fi
+    elif [ "$DIRECTION" -eq 1 ]; then
+        CURRENT_ROW=$((CURRENT_ROW - 1))
+        set_pixel $CURRENT_ROW $CURRENT_COL
+        if (( CENTER_ROW - CURRENT_ROW >= COUNT )); then
+            ((DIRECTION++))
+        fi
+    elif [ "$DIRECTION" -eq 2 ]; then
+        CURRENT_COL=$((CURRENT_COL - 1))
+        set_pixel $CURRENT_ROW $CURRENT_COL
+        if (( CENTER_COL - CURRENT_COL >= 2 * COUNT )); then
+            ((DIRECTION++))
+        fi
+    elif [ "$DIRECTION" -eq 3 ]; then
+        CURRENT_ROW=$((CURRENT_ROW + 1))
+        set_pixel $CURRENT_ROW $CURRENT_COL
+        if (( CURRENT_ROW - CENTER_ROW >= COUNT )); then
+            ((DIRECTION++))
+        fi
+    else
+        COUNT=$((COUNT + 1))
+        CURRENT_ROW=$((CURRENT_ROW + 1))
+        if (( COUNT > CENTER_ROW )); then
+            COUNT=0
+            CURRENT_ROW=$CENTER_ROW
+        fi
+        set_pixel $CURRENT_ROW $CURRENT_COL
+        DIRECTION=0
+    fi
+}
+
+# Main loop
+tput civis
+trap "tput cnorm; exit" INT
+
+CURRENT_ROW=$CENTER_ROW
+CURRENT_COL=$CENTER_COL
+DIRECTION=0
+COUNT=1
+COLOURS=(1 2 3)
 
 while true; do
-    for ((i=0; i<START_ROW - 1; i++)); do
-
-        ROW=$((START_ROW + i))
-        for ((j=-i * 2 + 2; j<i * 2; j += 2)); do
-            COLUMN=$((START_COL + j))
-
-            tput cup $ROW $COLUMN
-            echo -e "=="
-        done
-
-        MOD_COLUMN=$((START_COL + i * 2))
-        for ((j=-i; j<i; j++)); do
-            MOD_ROW=$((START_ROW - j))
-            tput cup $MOD_ROW $MOD_COLUMN
-           echo -e "=="
-        done
-
-        MOD_ROW=$((START_ROW - i))
-        for ((j=-i * 2; j<i * 2; j += 2)); do
-            MOD_COLUMN=$((START_COL - j))
-
-            tput cup $MOD_ROW $MOD_COLUMN
-            echo -e "=="
-        done
-
-        MOD_COLUMN=$((START_COL - i * 2))
-        for ((j=-i; j<i + 1; j++)); do
-            MOD_ROW=$((START_ROW + j))
-
-            tput cup $MOD_ROW $MOD_COLUMN
-            echo -e "=="
-        done
-        
-        sleep $DELAY
-    done
-
-    COLOR_INDEX=$(((COLOR + 1) % ${#COLORS[@]}))
-    COLOR=${COLORS[$COLOR_INDEX]}
-
-    tput setaf $COLOR
-
-    tput cup $((START_ROW + HEIGHT - 1)) $START_COL
-    echo "$(printf ' %.0s' $(seq 1 $LINE_LENGTH))"
+    start_spiral
+    render
 done
 
-# Restore cursor (in case trap doesn't fire)
 tput cnorm
+
