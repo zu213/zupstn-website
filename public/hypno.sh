b3539b844b50a369c856b9eb89fb1c361f75df66
Zach Upstone <zu213@bath.ac.uk>
2025-05-19 21:48:42 +0100

    Clean curl animation

--- public/hypno.sh
+++ public/hypno.sh
@@ -4,22 +4,21 @@ ROWS=$(tput lines)
 COLS=$(tput cols)
 CENTER_ROW=$((ROWS / 2))
 CENTER_COL=$((COLS / 2))
+stty -echo
+trap "stty echo; tput cnorm; exit" INT
+tput reset
 tput civis
-trap "tput cnorm; exit" INT
-
 
 render() {
-    for ((k = 0; k < 3; k++)); do
+    for ((k = 0; k < 5; k++)); do
         row=${CURRENT_ROWS[$k]}
         col=${CURRENT_COLS[$k]}
         color=${COLOURS[$k]}
         
-        # ANSI: \033[row;colH moves cursor, \033[3Xm sets color
+        # ANSI: print for spped
         printf "\033[%s;%sH\033[3%sm==\033[0m" "$row" "$col" "$color"
     done
 }
-
-# Spiral drawing into buffer
 
 animate_spiral() {
     local spiral=$1
@@ -52,27 +51,26 @@ animate_spiral() {
         if (( CURRENT_ROWS[spiral] > ROWS )); then
             COUNTS[$spiral]=0
             CURRENT_ROWS[$spiral]=$CENTER_ROW
+            CURRENT_COLS[$spiral]=$CENTER_COL
         fi
         DIRECTIONS[$spiral]=0
     fi
 
 }
 
-# Main loop
-tput civis
-trap "tput cnorm; exit" INT
-
-CURRENT_ROWS=($((CENTER_ROW)) $((CENTER_ROW + 2)) $((CENTER_ROW + 5)))
-CURRENT_COLS=($CENTER_COL $CENTER_COL $CENTER_COL)
-DIRECTIONS=(0 0 0)
-COUNTS=(0 2 5)
-COLOURS=(1 2 3)
+CURRENT_ROWS=($((CENTER_ROW)) $((CENTER_ROW + 5)) $((CENTER_ROW + 8))  $((CENTER_ROW + 10))  $((CENTER_ROW + 11)))
+CURRENT_COLS=($CENTER_COL $((CENTER_COL)) $((CENTER_COL)) $((CENTER_COL)) $((CENTER_COL)))
+DIRECTIONS=(0 0 0 0 0)
+COUNTS=(0 5 8 10 11)
+COLOURS=(1 2 3 4 5)
 
 while true; do
     animate_spiral 0
     animate_spiral 1
     animate_spiral 2
-    
+    animate_spiral 3
+    animate_spiral 4
+
     render
     sleep 0.01
 done
