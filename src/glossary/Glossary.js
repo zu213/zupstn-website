eaf3b3a3426257feeb078c1fb7e9efa3742a8f63
Zach Upstone <zu213@bath.ac.uk>
2025-08-01 08:53:05 +0100

    Rearranged glossary page

--- src/glossary/Glossary.js
+++ src/glossary/Glossary.js
@@ -4,18 +4,22 @@ import './Glossary.css';
 function Glossary() {
   return (
     <div>
-      <div className='linkTitle'>Page links</div>
-      <div className='linksContainer'>
-        <div className='link'> <Link to="/">/home</Link></div>
-        <div className='link'> <Link to="/me">/me</Link></div>
-        <div className='link'> <Link to="/cv">/cv</Link></div>
-        <div className='link'> <Link to="/projects">/projects</Link></div>
-        <div className='link'> <Link to="/gallery">/gallery</Link></div>
-        <div className='link'> <Link to="/charts">/charts</Link></div>
-        <div className='link'> <Link to="/dissertation-tool">/dissertation-tool (under maintenance)</Link></div>
-        <div className='link'> <Link to="/fruit-lips">/fruit-lips</Link></div>
-        <div className='link'> <Link to="/368squares">/368squares</Link></div>
-        <div className='link'> <Link to="/llm-compare">/llm-compare</Link></div>
+      <div className='pageHeader'>Page links</div>
+      <div className='linksListContainer'>
+        <div className='linksContainer'>
+          <div className='link'> <Link to="/">/home</Link></div>
+          <div className='link'> <Link to="/me">/me</Link></div>
+          <div className='link'> <Link to="/cv">/cv</Link></div>
+          <div className='link'> <Link to="/projects">/projects</Link></div>
+          <div className='link'> <Link to="/gallery">/gallery</Link></div>
+        </div>
+        <div className='linksContainer'>
+          <div className='link'> <Link to="/charts">/charts</Link></div>
+          <div className='link'> <Link to="/dissertation-tool">/dissertation-tool (inactive)</Link></div>
+          <div className='link'> <Link to="/fruit-lips">/fruit-lips</Link></div>
+          <div className='link'> <Link to="/368squares">/368squares</Link></div>
+          <div className='link'> <Link to="/llm-compare">/llm-compare</Link></div>
+        </div>
       </div>
     </div>
   );
