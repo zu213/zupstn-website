e8d4f7e453f33819e3a98ceb9451135ad957b765
Zach Upstone <zu213@bath.ac.uk>
2025-08-16 15:49:06 +0100

    Updated CV

--- src/cv/CV.js
+++ src/cv/CV.js
@@ -26,16 +26,15 @@ function CV() {
 
   return (
     <div className='cv-page'>
-      <br/>
-      <a className="small-link" href="/documents/CV_short.pdf" download="Zachary Upstone CV">
-        Download <img className="download-image" src={download} alt="Download"></img>
-      </a>
-          &nbsp; | &nbsp;
-      <a className="small-link" href="/documents/CV.pdf" download="Zachary Upstone CV">
-        Extended CV <img className="download-image" src={download} alt="Download"></img>
-      </a>
-      <br/>
-      <br/>
+      <div className='cv-page-links'>
+        <a className="small-link" href="/documents/CV_short.pdf" download="Zachary Upstone CV">
+          Download <img className="download-image" src={download} alt="Download"></img>
+        </a>
+            &nbsp; | &nbsp;
+        <a className="small-link" href="/documents/CV.pdf" download="Zachary Upstone CV">
+          Extended CV <img className="download-image" src={download} alt="Download"></img>
+        </a>
+      </div>
       <div className='too-small-cv'> Screen size is too small to display pdf, download to view.</div>
       <div className='safari-cv'> Safari doesn't allow proper display of pdfs.</div>
   
