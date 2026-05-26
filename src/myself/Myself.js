255bcebfe58235690723d9b04464ae1a481986d6
zu213 <hcazenotspu@gmail.com>
2026-05-26 19:20:44 +0100

    Updated me page on site

--- src/myself/Myself.js
+++ src/myself/Myself.js
@@ -19,30 +19,28 @@ function Me() {
         <FadeInSection>
           <div className='subsub-title'>
             <hr />
-              Programmer
+              Product Developer
             <hr />
           </div>
-          
-            Find my GitHub <a className="small-link" href="https://github.com/zu213" target="_blank" rel="noreferrer">here</a> -  it includes many of my personal projects, which can also be seen on the&nbsp;
-          <BreadcrumbLink className="small-link" to="/projects">Projects page</BreadcrumbLink>. I am particularly interested in graphics, alongside my work in native and web development.
+            My <a className="small-link" href="https://github.com/zu213" target="_blank" rel="noreferrer">GitHub</a> contains many of my personal projects, several of which are featured on the&nbsp;
+            <BreadcrumbLink className="small-link" to="/projects">Projects page</BreadcrumbLink>. I am particularly interested in graphics, alongside my work in native and web development.
           <br/>
           <br/>
       
-            I am a <b>Product developer</b> for&nbsp;   
+            I am a <b>Product Developer</b> for&nbsp;   
           <a className="small-link" href="https://www.pugpig.com/" target='_blank' rel='noreferrer'>Pugpig</a> a media publishing platform powering 
-            apps and web readers. My work here spans <b>Native</b><i>(SwiftUI, Swift, Objective-C, Java, Kotlin)</i> and <b>Web</b><i>(Vue, PHP)</i> development.
+            apps and web readers. My work here spans <b>Native</b><i>(iOS/Android)</i> and <b>Web</b><i>(Vue, PHP)</i> development, where I have introduced new features, modernised legacy code, contributed to TypeScript migration efforts, and helped improve the experience for both developers and end users.
+
+            <br />
+            <br />
             Previously, I completed a year-long placement at &nbsp;
           <a className="small-link" href="https://www.kontron-americas.com/" target='_blank' rel='noreferrer'>BSQUARE</a>, an IoT solutions company, 
             where I worked in <b>Software Development</b> and <b>QA</b>, gaining experience with <b>Angular</b><i>(TS, HTML, SASS)</i>,
             Gherkin and Selenium.
-        </FadeInSection>
-        <FadeInSection>
-          <div className='subsub-title'>
-            <hr />
-              Graduate
-            <hr />
-          </div>
-            I graduated from the University of Bath with a <b>First Class honours degree in Computer Science(BSc Hons)</b>.
+          
+          <br />
+          <br />
+            I graduated from the University of Bath with a <b>First Class honours degree in Computer Science (BSc Hons)</b>.
             My degree focused on AI, the mathematics underpinning computer science, graphics processing and modern software development methodologies.
         </FadeInSection>
         <FadeInSection>
@@ -59,7 +57,11 @@ function Me() {
             Additionally
             <hr />
           </div>
-            If you are interested professionally add me on <a className='small-link' href='https://www.linkedin.com/in/zachary-upstone-076218214/' target='_blank' rel='noreferrer'>LinkedIn</a>. Feel free to navigate the rest of my website by visiting the <BreadcrumbLink className="small-link" to="/glossary">Glossary page</BreadcrumbLink> - many of the pages are hidden otherwise!
+            If you are interested professionally connect with me on <a className='small-link' href='https://www.linkedin.com/in/zachary-upstone-076218214/' target='_blank' rel='noreferrer'>LinkedIn</a>, 
+            or feel free to browse my <BreadcrumbLink className='small-link' to='/cv'>CV</BreadcrumbLink> page which is powered by AI to automatically update alongside the pdf.
+            <br />
+            <br />
+            Feel free to navigate the rest of my website by visiting the <BreadcrumbLink className="small-link" to="/glossary">Glossary page</BreadcrumbLink> - many of the pages are hidden otherwise!
         </FadeInSection>
       </div>
     </div>
