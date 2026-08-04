ed5e0035df11f11099413e669e1c985e2e7cc94a
zu213 <hcazenotspu@gmail.com>
2026-08-04 17:11:22 +0100

    tweaked build sccript to use install

--- build-script.sh
+++ build-script.sh
@@ -4,12 +4,12 @@
   SUBMODULE="public/spotify-roulette/frontend"
 
   echo "→ Building spotify-roulette submodule…"
-  npm --prefix "$SUBMODULE" ci
+  npm --prefix "$SUBMODULE" install
   CI=false npm --prefix "$SUBMODULE" run build
   rm -rf "$SUBMODULE/node_modules"   # keep it out of the copied static output
 
   echo "→ Building site…"
-  npm ci
+  npm install
   npm run build
 
   echo "✓ Done."
